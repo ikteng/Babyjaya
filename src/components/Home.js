ccca59d6bfe1e025b16f3ee84bc590786d641d0b
Ik Teng <154477174+ikteng@users.noreply.github.com>
2024-10-26 22:39:21 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import './Home.css'; // Make sure to create this CSS file for styling
 const Home = () => {
     // Example product data (ensure the paths are correct)
     const products = [
-        { id: 1, img: "/assets/baby-clothes.jpg", title: "Baby Clothes", description: "Stylish and comfortable clothes for your little one." },
-        { id: 2, img: "/assets/baby-toys.jpg", title: "Toys", description: "Fun and educational toys to stimulate your baby's growth." },
-        { id: 3, img: "/assets/baby-cot.jpg", title: "Baby Cot Bed", description: "A cozy and safe sleeping space for your little one, designed to provide comfort and support for a good night's sleep." },
+        { id: 1, img: "https://github.com/ikteng/babyjaya/blob/470837a8ce603688a5c1102d2680d0a21394073e/public/assets/baby-clothes.jpg", title: "Baby Clothes", description: "Stylish and comfortable clothes for your little one." },
+        { id: 2, img: "https://github.com/ikteng/babyjaya/blob/470837a8ce603688a5c1102d2680d0a21394073e/public/assets/assets/baby-toys.jpg", title: "Toys", description: "Fun and educational toys to stimulate your baby's growth." },
+        { id: 3, img: "https://github.com/ikteng/babyjaya/blob/470837a8ce603688a5c1102d2680d0a21394073e/public/assets/assets/baby-cot.jpg", title: "Baby Cot Bed", description: "A cozy and safe sleeping space for your little one, designed to provide comfort and support for a good night's sleep." },
     ];
 
     return (
@@ -15,7 +15,7 @@ const Home = () => {
             <div className="banner-container">
                 <div className="banner">
                     <img 
-                        src="/assets/banner.png" // Ensure the banner image path is also correct
+                        src="https://github.com/ikteng/babyjaya/blob/470837a8ce603688a5c1102d2680d0a21394073e/public/assets/assets/banner.png" // Ensure the banner image path is also correct
                         alt="BabyJaya, A One-Stop Shop for Your Baby" 
                         className="banner-image" 
                     />
